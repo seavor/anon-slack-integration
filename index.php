39afa5a7ce8968297b1073150fd306890fc4869d
Jeremy Letto <jletto87@gmail.com>
2016-03-31 13:26:22 -0400

    add test

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
     require 'vendor/autoload.php';
-    
+
     $app = new \Slim\Slim();
-    
+
+
     $app->post('/message', function() use ($app) {
+        /*
+          Example request body: {
+            token: HDJ3q24J5634JBGhf3sfs32S
+            team_id: T0001
+            team_domain: example
+            channel_id: C2147483705
+            channel_name: test
+            user_id: U2147483697
+            user_name: Steve
+            command: /weather
+            text: 94070
+            response_url: https://hooks.slack.com/commands/1234/5678
+          }
+        */
         $body = $app->request->getBody();
 
-        echo $body;
+        echo json_stringify($body);
     });
 
     $app->run();
